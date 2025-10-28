95a82aaab5cf8180e1347a723d5bc2533f3d97ef
Prince Pandey <work.princepandey@gmail.com>
2025-10-29 00:43:35 +0530

    added root dir

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,18 @@ import { Signin } from "./pages/Signin";
 import { Signup } from "./pages/Signup";
 import { Blog } from "./pages/Blog";
 import { Blogs } from "./pages/Blogs";
-import {Publish} from "./pages/Publish"
+import { Publish } from "./pages/Publish";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Blogs />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
           <Route path="/blogs" element={<Blogs />} />
-          <Route path="/publish" element={<Publish/>}/>
+          <Route path="/publish" element={<Publish />} />
         </Routes>
       </BrowserRouter>
     </>
