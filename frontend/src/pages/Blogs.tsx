5019546b2dcab9089b2b3c61faccf33c693c0b37
= <=>
2025-10-28 23:47:29 +0530

    added backend

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -12,7 +12,7 @@ export const Blogs = () => {
     return (
       <div>
         <Appbar />
-        <div className="flex justify-center">
+        <div className="flex justify-ce">
           <BlogSkelton />
           <BlogSkelton />
           <BlogSkelton />
